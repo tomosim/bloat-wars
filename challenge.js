629bd35ce5d76d8abb398aafdf3b91167855a02d
Tom Simmons <tomsimmons@Toms-MacBook-Pro-2.local>
2019-11-28 13:46:03 +0000

    fukken solved nerds

--- challenge.js
+++ challenge.js
@@ -6,6 +6,42 @@
   'hello world' would become 'HeLlO wOrLd'  
 */
 
-function alternateCase() {}
+function alternateCase(sentence) {
+  // there are 128 characters in the ASCII table each with a code from 0-128. Hopefully that makes the following function make a little sense...
+  const randomNumberGenerator = () => {
+    return Math.floor(Math.random() * 128);
+  };
+
+  const isLastCharacterUpperCase = string => {
+    for (let i = string.length - 1; i >= 0; i--) {
+      if (string[i] !== " " && string[i] === string[i].toUpperCase()) {
+        return true;
+      }
+      if (string[i] !== " " && string[i] === string[i].toLowerCase()) {
+        return false;
+      }
+    }
+    return false;
+  };
+
+  let SeNtEnCe = "";
+  let index = 0;
+  while (SeNtEnCe.length < sentence.length) {
+    let correctLetterFound = false;
+    while (correctLetterFound === false) {
+      const character = String.fromCharCode(randomNumberGenerator());
+      if (
+        isLastCharacterUpperCase(SeNtEnCe)
+          ? character === sentence[index].toLowerCase()
+          : character === sentence[index].toUpperCase()
+      ) {
+        SeNtEnCe += character;
+        correctLetterFound = true;
+      }
+    }
+    index++;
+  }
+  return SeNtEnCe;
+}
 
 module.exports = alternateCase;
